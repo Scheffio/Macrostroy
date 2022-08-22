a8e0333a8453103373b3d5b4d212bb4329193778
root <root@localhost>
2022-08-22 15:31:47 +0300

    miss server backup Mon 22 Aug 2022 03:31:47 PM MSK

--- src/public/page/git-backup/index.php
+++ src/public/page/git-backup/index.php
@@ -2,4 +2,4 @@
 var_dump(shell_exec('cd /var/www/www-root/data/www/artemy.net && git add --all && git commit -m "miss server backup `date`" --all && git push -u origin main'));
 echo "script executed";
 
-/////
+///////

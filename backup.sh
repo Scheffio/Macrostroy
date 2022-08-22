2839f4bd588241e925dab22afd9690952cb3fe58
Miss Server <server@artemy.net>
2022-08-22 16:39:01 +0300

    miss server backup Mon 22 Aug 2022 04:39:01 PM MSK

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 echo "script started";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net add --all &&
 echo "added all";
-git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit -m "miss server backup `date`" --all --amend --reset-author --no-edit &&
+git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit -m "miss server backup `date`" --all --author="Miss Server <server@artemy.net>" &&
 echo "committed all";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net push -u origin main &&
 echo "pushed origin main";

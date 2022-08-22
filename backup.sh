fdaa23a8e7793e9143a1043898206d8518f3989e
root <root@localhost>
2022-08-22 16:08:36 +0300

    miss server backup Mon Aug 22 16:08:36 MSK 2022

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "script started";
-git config --global user.name "Miss Server" &&
+git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net config --global user.name "Miss Server" &&
 echo "Configured name as Miss Server";
-git config --global user.email server@artemy.net &&
+git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net config --global user.email server@artemy.net &&
 echo "Configured email as server@artemy.net";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net add --all &&
 echo "added all";

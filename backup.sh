6cabf07f81644a6dc2a56110abccea8134653e0f
Miss Server <server@artemy.net>
2022-08-22 16:31:56 +0300

    miss server backup Mon Aug 22 16:31:56 MSK 2022

--- backup.sh
+++ backup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 echo "script started";
-#git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net config --global user.name "Miss Server" &&
-#echo "Configured name as Miss Server";
-#git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net config --global user.email server@artemy.net &&
-#echo "Configured email as server@artemy.net";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net add --all &&
 echo "added all";
-git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit -m "miss server backup `date`" --all &&
+git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit -m "miss server backup `date`" --all --author="Miss Server <server@artemy.net>" &&
 echo "committed all";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net push -u origin main &&
 echo "pushed origin main";
-git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit --amend --reset-author
-echo "amened author";
 echo "script executed";

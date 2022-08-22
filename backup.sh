817432e695ed35fe496347cc384488652da3c95d
root <root@localhost>
2022-08-22 16:08:13 +0300

    miss server backup Mon Aug 22 16:08:13 MSK 2022

--- backup.sh
+++ backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "script started";
+git config --global user.name "Miss Server" &&
+echo "Configured name as Miss Server";
+git config --global user.email server@artemy.net &&
+echo "Configured email as server@artemy.net";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net add --all &&
 echo "added all";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit -m "miss server backup `date`" --all &&

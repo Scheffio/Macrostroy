2961855353fb0df5f1a743213c72590d8bfc9009
Miss Server <server@artemy.net>
2022-08-22 16:44:14 +0300

    miss server backup Mon Aug 22 16:44:14 MSK 2022

--- backup.sh
+++ backup.sh
@@ -4,9 +4,6 @@ git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/ww
 echo "added all";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit -m "miss server backup `date`" --all --author="Miss Server <server@artemy.net>" &&
 echo "committed all";
-git config --global user.name "you name"
-git config --global user.email you@domain.example
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net push -u origin main &&
-git commit --amend --reset-author
 echo "pushed origin main";
 echo "script executed";

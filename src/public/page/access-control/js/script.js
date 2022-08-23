979fc2011992a3a95fb02c2fd890b674ade675be
Miss Server <server@artemy.net>
2022-08-23 16:00:01 +0300

    miss server backup Tue 23 Aug 2022 04:00:01 PM MSK

--- src/public/page/access-control/js/script.js
+++ src/public/page/access-control/js/script.js
@@ -1,37 +1,3 @@
-fetch("/api/v1/users", {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-      }
-})
-.then(function(res) {
-    return res.json();
-})
-.then(function(json) {
-    json.data.forEach((elem) => {
-        Object.values(elem).forEach((item) => {
-            if(item != "" && typeof item == "string") {
-                document.querySelector('.users__list').appendChild(users.createElement('div', 'users__user-field', '', `<p>${item}</p>`))
-            }
-        })
-    })
-
-    const selectableUsers = {
-        users: document.querySelectorAll('.users__user-field'),
-        click(elem) {
-            this.reset()
-            elem.classList.toggle('selected')
-        },
-        reset() {
-            this.users.forEach((elem) => {
-                elem.classList.remove('selected')
-            })
-        }
-    }
-    
-    document.querySelectorAll('.users__user-field').forEach((elem) => {elem.addEventListener('click', () => {selectableUsers.click(elem)})})
-})
-
 // Transfer project name
 function setItemName() {
     if(localStorage.getItem('item-name')) {
@@ -54,31 +20,36 @@ checkboxes.forEach((elem) => {
 
 // Make fields clicked
 
-
-// ===================
-
-// parse users 
-
-const users = {
-    createElement(element, elementClass, elementModificator, elementContent) {
-        let create = document.createElement(`${element}`)
-        if (elementClass != '' && elementModificator != '') {
-            create.classList.add(`${elementClass}`)
-            create.classList.add(`${elementModificator}`)
-        } else if (elementClass != '') {
-            create.classList.add(`${elementClass}`)
-        }
-        if (elementContent != '') {
-            create.innerHTML = elementContent
-        }
-        return create
-        // элементо-генератор
+const selectableUsers = {
+    users: document.querySelectorAll('.users__user-field'),
+    click(elem) {
+        this.reset()
+        elem.classList.toggle('selected')
+    },
+    reset() {
+        this.users.forEach((elem) => {
+            elem.classList.remove('selected')
+        })
     }
 }
 
-
+document.querySelectorAll('.users__user-field').forEach((elem) => {elem.addEventListener('click', () => {selectableUsers.click(elem)})})
 // ===================
 
-titleChecker.resetClasses()
-titleChecker.checkTitle(document.title)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 setItemName()

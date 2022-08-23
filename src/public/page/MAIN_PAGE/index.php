898f4e4898448c62362ab50e9d42dddf2d61968c
Miss Server <server@artemy.net>
2022-08-23 12:00:01 +0300

    miss server backup Tue 23 Aug 2022 12:00:01 PM MSK

--- src/public/page/MAIN_PAGE/index.php
+++ src/public/page/MAIN_PAGE/index.php
@@ -12,77 +12,293 @@
     <link rel="mask-icon" href="../auth/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#00aba9">
     <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="style.css">
     <title>Estimin - Главная</title>
-    <?php require __DIR__ . "/../../../inc/timur/header-styles.php" ?>
 </head>
 
-<body>
+<body class="body">
     <!-- HEADER -->
-    <?php require __DIR__ . "/../../../inc/timur/header.php"; ?>
     <!-- ================= -->
     <div class="wrap">
         <div class="tabs-controller">
             <div class="tabs-controller__tab-anchor">
-                <a href="#1-tab-element" class="tab-anchor__anchor-name selected">ЖК</a>
+                <a href="#cities" class="tab-anchor__anchor-name selected">Города</a>
             </div>
             <div class="tabs-controller__tab-anchor">
-                <a href="#2-tab-element" class="tab-anchor__anchor-name">Города</a>
+                <a href="#residential-complexes" class="tab-anchor__anchor-name">ЖК</a>
             </div>
             <div class="tabs-controller__tab-anchor">
-                <a href="#3-tab-element" class="tab-anchor__anchor-name">Дома</a>
+                <a href="#houses" class="tab-anchor__anchor-name">Дома</a>
             </div>
             <div class="tabs-controller__tab-anchor">
-                <a href="#4-tab-element" class="tab-anchor__anchor-name">Этапы</a>
+                <a href="#stages" class="tab-anchor__anchor-name">Этапы</a>
             </div>
         </div>
         <div class="tabs-container">
-            <div id="1-tab-element" class="tabs_container__tab-element">
+            <div id="cities" class="tabs_container__tab-element">
                 <div class="tab-header">
                     <div class="tab-header__element">
                         <div class="sorter">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по убыванию">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по возрастанию">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
                         </div>
                         <p>Название</p>
                     </div>
                     <div class="tab-header__element">
                         <div class="sorter">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по убыванию">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по возрастанию">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
                         </div>
                         <p>Статус</p>
                     </div>
                     <div class="tab-header__element">
                         <div class="sorter">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по убыванию">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по возрастанию">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
                         </div>
                         <p>Доступ</p>
                     </div>
                     <div class="tab-header__element">
                         <div class="sorter">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по убыванию">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по возрастанию">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
                         </div>
                         <p>Последнее изменение</p>
                     </div>
                     <div class="tab-header__element">
                         <div class="sorter">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по убыванию">
-                            <img src="../../static/sorter_icons/sorter-default.svg" alt="Кнопка сортировки по возрастанию">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
                         </div>
                         <p>Общая стоимость</p>
                     </div>
                 </div>
             </div>
-            <div id="2-tab-element" class="tabs_container__tab-element">
-                <p>test2</p>
+            <div id="residential-complexes" class="tabs_container__tab-element">
+                <div class="tab-header">
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Название</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Статус</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Доступ</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Последнее изменение</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Общая стоимость</p>
+                    </div>
+                </div>
             </div>
-            <div id="3-tab-element" class="tabs_container__tab-element">
-                <p>test 3</p>
+            <div id="houses" class="tabs_container__tab-element">
+                <div class="tab-header">
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Название</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Статус</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Доступ</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Последнее изменение</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Общая стоимость</p>
+                    </div>
+                </div>
             </div>
-            <div id="4-tab-element" class="tabs_container__tab-element">
-                <p>test 4</p>
+            <div id="stages" class="tabs_container__tab-element">
+                <div class="tab-header">
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Название</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Статус</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Доступ</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Последнее изменение</p>
+                    </div>
+                    <div class="tab-header__element">
+                        <div class="sorter">
+                            <img src="../../static/icons/sorter/sorter-default.svg" alt="Кнопка сортировки по убыванию">
+                            <img src="../../static/icons/sorter/sorter-default.svg"
+                                alt="Кнопка сортировки по возрастанию">
+                        </div>
+                        <p>Общая стоимость</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="add-project">
+            <button class="add-project__button" onclick="modalSystem.showModalWindow()"><span class="plus"></span>Добавить</button>
+        </div>
+    </div>
+    <nav class="context-menu">
+        <ul class="context-menu__items">
+            <li class="context-menu__item open" onclick="CCM.contextMenu_open(CCM.rowInFocus)">
+                <a>Открыть</a>
+            </li>
+            <li class="context-menu__item copy" onclick="CCM.contextMenu_copy(CCM.rowInFocus)">
+                <a>Копировать</a>
+            </li>
+            <li class="context-menu__item disabled paste" onclick="CCM.contextMenu_paste()">
+                <a>Вставить</a>
+            </li>
+            <li class="context-menu__item add" onclick="CCM.contextMenu_add()">
+                <a>Добавить</a>
+            </li>
+            <li class="context-menu__item status-submenu change-status">
+                <a href="#cities">Изменить статус</a>
+                <ul class="status-context-sub-menu">
+                    <li class="status-context-sub-menu__item"
+                        onclick="CCM.contextMenu_changeStatus(this, CCM.rowInFocus)">
+                        <a>В процессе</a>
+                    </li>
+                    <li class="status-context-sub-menu__item"
+                        onclick="CCM.contextMenu_changeStatus(this, CCM.rowInFocus)">
+                        <a>Завершён</a>
+                    </li>
+                    <li class="status-context-sub-menu__item"
+                        onclick="CCM.contextMenu_changeStatus(this, CCM.rowInFocus)">
+                        <a>Заморожен</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="context-menu__item private" onclick="CCM.contextMenu_private(CCM.rowInFocus)">
+                <a>Сделать приватным</a>
+            </li>
+            <li class="context-menu__item access-control" onclick="CCM.contextMenu_accessControl(CCM.rowInFocus)">
+                <a href="../access-control">Управление доступом</a>
+            </li>
+            <li class="context-menu__item make-estimate">
+                <a>Сформировать смету</a>
+            </li>
+            <li class="context-menu__item about">
+                <a>История действий</a>
+            </li>
+            <li class="context-menu__item delete" onclick="CCM.contextMenu_delete(CCM.rowInFocus)">
+                <a>Удалить</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="modal">
+        <div class="modal-wrap">
+            <span onclick="modalSystem.hideModalWindow()"></span>
+            <div class="modal-header">
+                <p class="title">Добавить проект</p>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="modal-body__name">
+                        <input type="text">
+                    </div>
+                    <div class="modal-body__status">
+                        <select>
+                            <option value="В процессе">В процессе</option>
+                            <option value="Завершён">Завершён</option>
+                            <option value="Заморожен">Заморожен</option>
+                        </select>
+                    </div>
+                    <div class="modal-body__access">
+                        <label for="access">Доступ</label>
+                        <div class="checkboxes">
+                            <div class="public">
+                                <input type="radio" name="access-radio" id="public">
+                                <label for="public">Открытый</label>
+                            </div>
+                            <div class="private">
+                                <input type="radio" name="access-radio" id="private">
+                                <label for="private">Публичный</label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button>Добавить</button>
             </div>
         </div>
     </div>

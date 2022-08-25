1fa67973d9747ad565aeb33b40d64ee2afa60ac7
Miss Server <server@artemy.net>
2022-08-25 16:00:01 +0300

    miss server backup Thu 25 Aug 2022 04:00:01 PM MSK

---
+++ src/inc/wipe/v1/volumes/Volumes.php
@@ -0,0 +1,12 @@
+<?php
+namespace wipe\inc\v1\volumes;
+
+class Volumes
+{
+    protected $obj;
+    protected $name;
+    protected $price;
+    protected $amount;
+    protected $is_available;
+
+}

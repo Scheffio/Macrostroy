aafd59364f16c08503e712f4b0061b001cc379bf
Miss Server <server@artemy.net>
2022-08-25 14:30:01 +0300

    miss server backup Thu 25 Aug 2022 02:30:01 PM MSK

--- src/public/page/access-control/js/script.js
+++ src/public/page/access-control/js/script.js
@@ -1,39 +1,47 @@
-let obj = {lvl: 1, object_id: 1, project_id: 1}
-fetch("/api/v1/users".search = new URLSearchParams(obj).toString(), {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-      }
-    // body: JSON.stringify({lvl: 1, object_id: 1, project_id: 1})
-})
-.then(function(res) {
-    return res.json();
-})
-.then(function(json) {
+let url = new URL('https://artemy.net/api/v1/users')
+let obj = {lvl: 1, object_id: 1}
+url.search = new URLSearchParams(obj).toString()
+
+fetch(url).then((elem) => {
+    return elem.json()
+}).then((json) => {
     console.log(json);
-    // json.data.forEach((elem) => {
-    //     Object.values(elem).forEach((item) => {
-    //         if(item != "" && typeof item == "string") {
-    //             document.querySelector('.users__list').appendChild(users.createElement('div', 'users__user-field', '', `<p>${item}</p>`))
-    //         }
-    //     })
-    // })
-
-    // const selectableUsers = {
-    //     users: document.querySelectorAll('.users__user-field'),
-    //     click(elem) {
-    //         this.reset()
-    //         elem.classList.toggle('selected')
-    //     },
-    //     reset() {
-    //         this.users.forEach((elem) => {
-    //             elem.classList.remove('selected')
-    //         })
-    //     }
-    // }
-
-    // document.querySelectorAll('.users__user-field').forEach((elem) => {elem.addEventListener('click', () => {selectableUsers.click(elem)})})
 })
+// fetch("/api/v1/users".search = new URLSearchParams(obj).toString(), {
+//     method: 'GET',
+//     headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     // body: JSON.stringify({lvl: 1, object_id: 1, project_id: 1})
+// })
+// .then(function(res) {
+//     return res.json();
+// })
+// .then(function(json) {
+//     console.log(json);
+//     // json.data.forEach((elem) => {
+//     //     Object.values(elem).forEach((item) => {
+//     //         if(item != "" && typeof item == "string") {
+//     //             document.querySelector('.users__list').appendChild(users.createElement('div', 'users__user-field', '', `<p>${item}</p>`))
+//     //         }
+//     //     })
+//     // })
+
+//     // const selectableUsers = {
+//     //     users: document.querySelectorAll('.users__user-field'),
+//     //     click(elem) {
+//     //         this.reset()
+//     //         elem.classList.toggle('selected')
+//     //     },
+//     //     reset() {
+//     //         this.users.forEach((elem) => {
+//     //             elem.classList.remove('selected')
+//     //         })
+//     //     }
+//     // }
+
+//     // document.querySelectorAll('.users__user-field').forEach((elem) => {elem.addEventListener('click', () => {selectableUsers.click(elem)})})
+// })
 
 
 // Transfer project name

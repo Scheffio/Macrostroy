8016c86306383fb7a67ab11930b5067e8d0c0211
Miss Server <server@artemy.net>
2022-08-29 11:50:02 +0300

    miss server backup Mon 29 Aug 2022 11:50:02 AM MSK

--- src/public/api/v1/POST/object/add/index.php
+++ src/public/api/v1/POST/object/add/index.php
@@ -3,11 +3,28 @@
 
 use inc\artemy\v1\request\Request;
 use inc\artemy\v1\json_output\JsonOutput;
+use wipe\inc\v1\role\project_role\exception\IncorrectLvlException;
+use wipe\inc\v1\role\project_role\ProjectRole;
+use wipe\inc\v1\role\user_role\UserRole;
 
+$user = new UserRole();
 $request = new Request();
 
 try {
-//    objectName
+    if ($user->isManageUsers() && $user->isManageObjects()) {
+        throw new \wipe\inc\v1\objects\exception\AccessDeniedException();
+    }
+
+
+//    UserRole::getByUserId()->isManageUsersOrThrow();
+    $lvl = $request->getRequestOrThrow('lvl');
+
+    switch ($lvl) {
+        case ProjectRole::ATTRIBUTE_LVL_STR_PROJECT: break;
+        default: throw new IncorrectLvlException();
+    }
+
+    JsonOutput::success();
 } catch (Exception $e) {
     JsonOutput::success($e->getMessage());
 }

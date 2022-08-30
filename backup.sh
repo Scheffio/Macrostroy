3cccab7307c92ff7c57746aa3f2c5dd82d859a80
Miss Server <server@artemy.net>
2022-08-30 11:49:54 +0300

    miss server backup Tue Aug 30 11:49:54 MSK 2022

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "script started";
 sudo mysqldump --skip-dump-date macrostroy_db > /var/www/www-root/data/www/artemy.net/mysql_backup.sql &&
-echo "mysql backup completed"
+echo "mysql backup completed";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net add --all &&
 echo "files added";
 git --git-dir=/var/www/www-root/data/www/artemy.net/.git --work-tree=/var/www/www-root/data/www/artemy.net commit -m "miss server backup `date`" --all --author="Miss Server <server@artemy.net>" &&

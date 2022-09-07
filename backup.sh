8245cf78c26d49069a198ed8250cd451ba193463
Miss Server <server@artemy.net>
2022-09-07 10:22:53 +0300

    miss server backup Wed 07 Sep 2022 10:22:53 AM MSK

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "script started";
+rm /var/www/www-root/data/www/artemy.net/branches/main/mysql_backup.sql &&
 sudo mysqldump --skip-dump-date macrostroy_db > /var/www/www-root/data/www/artemy.net/branches/main/mysql_backup.sql &&
 echo "mysql backup completed";
 git --git-dir=/var/www/www-root/data/www/artemy.net/branches/main/.git --work-tree=/var/www/www-root/data/www/artemy.net/branches/main add --all &&

bae357b46495c27967881abd044ccd8035e7e029
Miss Server <server@artemy.net>
2022-09-21 15:20:03 +0300

    miss server backup Wed 21 Sep 2022 03:20:03 PM MSK

--- src/public/page/users/js/script.js
+++ src/public/page/users/js/script.js
@@ -191,7 +191,16 @@ const rolesControl = {
     },
     addRole() {
         // document.querySelector('.test').closest('div').previousElementSibling.children[0].children[0].value
-        fetch(`/api/v1/role/add?role_name=${document.querySelector('.role-name').value}`, {method: 'POST'})
+        fetch(`/api/v1/role/add`, {
+            method: 'POST', 
+            body: JSON.stringify({role_name})
+        })
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(json) {
+            console.log(json);
+        })
     }
 }
 
